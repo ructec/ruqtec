a6b5f0f6900debcfb064e0cc0343d41e02956b87
Johnson <ogbonnayajprof2015@gmail.com>
2023-08-24 13:42:17 +0100

    build faq page

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const accIcon = document.querySelectorAll(".acc_icon");
 const accBody = document.querySelectorAll(".acc1_body");
+const accIcon2 = document.querySelectorAll(".acc_icon2");
+const accBody2 = document.querySelectorAll(".acc2_body_text");
+
+console.log(accIcon2);
 
 for (let i = 0; i < accIcon.length; i++) {
   const eachIcon = accIcon[i];
@@ -17,5 +21,25 @@ for (let i = 0; i < accIcon.length; i++) {
     }
 
     //let's introduce state
+  });
+}
+for (let i = 0; i < accIcon2.length; i++) {
+  const eachIcon2 = accIcon2[i];
+  const eachBody2 = accBody2[i];
+  let isOpen;
+  eachIcon2.addEventListener("click", () => {
+    console.log("pp");
+    if (isOpen == true) {
+      eachBody2.classList.toggle("show2");
+      eachIcon2.classList.remove("fa-xmark");
+      eachIcon2.classList.add("fa-plus");
+      isOpen = false;
+    } else {
+      eachBody2.classList.toggle("show2");
+      eachIcon2.classList.remove("fa-plus");
+      eachIcon2.classList.add("fa-xmark");
+      // eachIcon2.classList.toggle("rotate");
+      isOpen = true;
+    }
   });
 }
