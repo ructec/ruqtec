a4852a205dba262e66501005c86c5bcf59cb23c9
Johnson <ogbonnayajprof2015@gmail.com>
2023-08-25 13:40:45 +0100

    build success message

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const accIcon = document.querySelectorAll(".acc_icon");
 const accBody = document.querySelectorAll(".acc1_body");
 const accIcon2 = document.querySelectorAll(".acc_icon2");
 const accBody2 = document.querySelectorAll(".acc2_body_text");
+const submitBtn = document.querySelector(".submit_btn");
+const regBody = document.querySelector(".reg_body");
+const regText = document.querySelector(".reg_text");
+const regHead = document.querySelector(".reg_head");
 
 for (let i = 0; i < accIcon.length; i++) {
   const eachIcon = accIcon[i];
@@ -39,3 +43,22 @@ for (let i = 0; i < accIcon2.length; i++) {
     }
   });
 }
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  regBody.innerHTML = `<div class="success_cont">
+      <div class="success_wrapper">
+        <div class="success_text_wrap">
+          <p class="success_text">Congratulations</p>
+        </div>
+        <div class="success_msg">
+          <p>Your form has been submitted</p>
+          <p>We’ve sent you a mail to know what’s next.</p>
+        </div>
+      </div>
+    </div>`;
+  regBody.style.height = "400px";
+  regText.style.color = "#EDEDED";
+  regHead.style.backgroundColor = "unset";
+  //   registerDiv.classList.toggle("format");
+});
