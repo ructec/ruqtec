2a0905c9e9e14858c1f189c612f7c8898d0d66ba
Johnson <ogbonnayajprof2015@gmail.com>
2023-08-28 23:33:49 +0100

    fix pop up code

--- pages/register/register.js
+++ pages/register/register.js
@@ -2,6 +2,7 @@ const submitBtn = document.querySelector(".submit_btn");
 const regBody = document.querySelector(".reg_body");
 const regText = document.querySelector(".reg_text");
 const regHead = document.querySelector(".reg_head");
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   regBody.innerHTML = `<div class="success_cont">
