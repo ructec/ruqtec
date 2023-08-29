7e92b06d66b68efbbb7fc5a43c05e3615474afbc
Johnson <ogbonnayajprof2015@gmail.com>
2023-08-29 06:50:19 +0100

    make accordion clickable in faq page

--- pages/faqs/faq.js
+++ pages/faqs/faq.js
@@ -1,10 +1,12 @@
 const accIcon2 = document.querySelectorAll(".acc_icon2");
 const accBody2 = document.querySelectorAll(".acc2_body_text");
+const aboutAcc2 = document.querySelectorAll(".about_accordion2");
 for (let i = 0; i < accIcon2.length; i++) {
   const eachIcon2 = accIcon2[i];
   const eachBody2 = accBody2[i];
+  const eachaboutAcc2 = aboutAcc2[i];
   let isOpen;
-  eachIcon2.addEventListener("click", () => {
+  eachaboutAcc2.addEventListener("click", () => {
     if (isOpen == true) {
       eachBody2.classList.toggle("show2");
       eachIcon2.classList.remove("fa-xmark");
