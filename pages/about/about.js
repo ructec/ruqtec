e0477b6a35b4b5920ba83a6a2daf64967442eff4
Johnson <ogbonnayajprof2015@gmail.com>
2023-08-29 06:38:30 +0100

    make accordion clickable in about page

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,11 +1,13 @@
 const accIcon = document.querySelectorAll(".acc_icon");
 const accBody = document.querySelectorAll(".acc1_body");
+const accHead = document.querySelectorAll(".acc1_head");
 
 for (let i = 0; i < accIcon.length; i++) {
   const eachIcon = accIcon[i];
   const eachBody = accBody[i];
+  const eachHead = accHead[i];
   let isOpen;
-  eachIcon.addEventListener("click", () => {
+  eachHead.addEventListener("click", () => {
     if (isOpen == true) {
       eachBody.classList.toggle("show");
       eachIcon.classList.toggle("rotate");
