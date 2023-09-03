7695ce98e859518120fcc84f545fc7328c5f95f1
Johnson <ogbonnayajprof2015@gmail.com>
2023-09-03 12:51:57 +0100

    add scroll to top feature

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
+document.querySelector(".top_btn").addEventListener("click", () => {
+  scrollToTop();
+});
