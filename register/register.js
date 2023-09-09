944eac00115da8c0d026100f47b66da6980183b5
Johnson <ogbonnayajprof2015@gmail.com>
2023-09-09 09:07:58 +0100

    add form features

--- register/register.js
+++ register/register.js
@@ -1,33 +1,18 @@
 const form = document.querySelector("form");
 const emailText = document.getElementById("emailtext");
 const emailInput = document.querySelector(".email_input");
+const loader = document.querySelector(".loader");
 // const submitBtn = document.querySelector(".submit_btn");
-// const allInputs = [...document.querySelectorAll(".each_input")];
-// const regBody = document.querySelector(".reg_body");
-// const regText = document.querySelector(".reg_text");
-// const regHead = document.querySelector(".reg_head");
-
-// submitBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   regBody.innerHTML = `<div class="success_cont">
-//       <div class="success_wrapper">
-//         <div class="success_text_wrap">
-//           <p class="success_text">Congratulations</p>
-//         </div>
-//         <div class="success_msg">
-//           <p>Your form has been submitted</p>
-//           <p>We’ve sent you a mail to know what’s next.</p>
-//         </div>
-//       </div>
-//     </div>`;
-//   regBody.style.height = "400px";
-//   regText.style.color = "#EDEDED";
-//   regHead.style.backgroundColor = "unset";
-// });
+const allInputs = [...document.querySelectorAll(".each_input")];
+const regBody = document.querySelector(".reg_body");
+const regText = document.querySelector(".reg_text");
+const regHead = document.querySelector(".reg_head");
 
 const api = "https://ruqtec-backend.vercel.app/api/auth/register";
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  loader.style.display = "block";
+  form.style.opacity = ".5";
   const formData = new FormData(form);
   //Convert array returned from formData to object
   const obj = Object.fromEntries(formData);
@@ -48,8 +33,35 @@ form.addEventListener("submit", (e) => {
     .then((data) => {
       console.log(data);
       if (data.message == "email already exists") {
+        loader.style.display = "none";
+        form.style.opacity = "1";
         emailText.style.display = "block";
         emailInput.style.border = "1px solid red";
+      } else {
+        emailText.style.display = "none";
+        emailInput.style.border = "unset";
+      }
+
+      if (data.message == "Registration successful") {
+        loader.style.display = "none";
+        form.style.opacity = "1";
+        allInputs.forEach((eachInput) => {
+          eachInput.value = "";
+        });
+        regBody.innerHTML = `<div class="success_cont">
+              <div class="success_wrapper">
+                <div class="success_text_wrap">
+                  <p class="success_text">Congratulations</p>
+                </div>
+                <div class="success_msg">
+                  <p>Your form has been submitted</p>
+                  <p>We’ve sent you a mail to know what’s next.</p>
+                </div>
+              </div>
+            </div>`;
+        regBody.style.height = "400px";
+        regText.style.color = "#EDEDED";
+        regHead.style.backgroundColor = "unset";
       }
     })
     .catch((err) => {
