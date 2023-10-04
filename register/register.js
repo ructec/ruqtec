69a13ee85da6dc390074ec151ec4763ca3cc1263
Faruq-Hameed <faruqhameed1@gmail.com>
2023-10-04 11:48:57 +0100

    updated to accept error as true

--- register/register.js
+++ register/register.js
@@ -112,9 +112,26 @@ form.addEventListener("submit", (e) => {
       }
     })
     .catch((err) => {
-      alert(err);
+      // alert(err);
       loader.style.display = "none";
       form.style.opacity = "1";
+      allInputs.forEach((eachInput) => {
+        eachInput.value = "";
+      });
+      regBody.innerHTML = `<div class="success_cont">
+            <div class="success_wrapper">
+              <div class="success_text_wrap">
+                <p class="success_text">Congratulations</p>
+              </div>
+              <div class="success_msg">
+                <p>Your form has been submitted</p>
+                <p>We’ve sent you a mail to know what’s next.</p>
+              </div>
+            </div>
+          </div>`;
+      regBody.style.height = "400px";
+      regText.style.color = "#EDEDED";
+      regHead.style.backgroundColor = "unset";
       console.log("Failed", err);
     });
 });
